f8621a9c820ef867a8495245aac4babe420ba401
PC <PC@DESKTOP-MCR33FO>
2022-07-20 19:23:38 -0500

    Hola commit

---
+++ src/main/java/com/mycompany/clasejuegos/persona.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.clasejuegos;
+
+/**
+ *
+ * @author PC
+ */
+public class persona {
+   private String nombre;
+   private String apellido;
+   private String edad;
+
+    public persona(String nombre, String apellido, String edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+   
+   
+    
+           
+           
+           
+}
